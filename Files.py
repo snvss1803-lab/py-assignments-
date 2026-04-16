970acf294b5610b81839f982e371083014e9ae7c
sanvissalunke <snvss1803@gmail.com>
2026-04-16 18:54:46 +0530

    Create Files.py

---
+++ Files.py
@@ -0,0 +1,28 @@
+#create file
+f=open("data.txt","w")
+f.write("hello\n")
+print("file created successfully!!")
+f.close()
+
+#read file
+f=open("data.txt")
+print(f.read())
+f.close()
+
+#append
+f=open("data.txt","a")
+f.write("how are you?")
+print("content appended successfully!!")
+f=open("data.txt")
+print(f.read())
+f.close()
+
+f=open("data.txt","r")
+print(f.read())
+
+
+f=open("data.txt","w")
+f.write("mit adt university")
+f=open("data.txt","r")
+print(f.read())
+f.close()
